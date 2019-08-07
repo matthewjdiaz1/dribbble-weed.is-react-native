f3c1c7e76498238e9b39c5164bef73d2a0e4872a
Matthew-Jacob-Diaz <matthewjdiaz1@gmail.com>
2019-08-07 16:26:36 -0700

    cleaned repo for react native

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// app.get('/', (req, res) => res.send('from express server'));
-app.use(express.static('public'));
-
-app.listen(port, () => console.log(`App listening on port ${port}.`));
